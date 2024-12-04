3dce2c2b707ff661bbfe72548d01d7c664bceaae
mireiahernandez <mireiahernandezcaralt@outlook.es>
2024-12-04 20:16:37 +0100

    Fix upload files example script to support new upload endpoint format of multiple uploads

--- examples/upload_files.py
+++ examples/upload_files.py
@@ -2,28 +2,35 @@
 import requests
 
 if __name__ == "__main__":
-    # pdf_path = "examples/ACME_earnings/ACME_Earnings.pdf"
     pdf_paths = [
         "examples/pdfs/Employee Handbook 2013-14.pdf",
         "examples/pdfs/ACME_Earnings.pdf"
     ]
+
+    files = []
     for pdf_path in pdf_paths:
-        with open(pdf_path, "rb") as file:
-            files = {
-                "file": (
-                    pdf_path.split("/")[-1],
-                    file,
-                    "application/pdf"
-                )
-            }
-            # db name is tenant1
-            db_name = "test_tenant1"
-            params = {
-                "db_name": db_name
-            }
-            response = requests.post(
-                "http://0.0.0.0:8000/upload/",
-                files=files,
-                params=params
+        files.append(
+            (
+                "files",
+                (pdf_path.split("/")[-1], open(pdf_path, "rb"), "application/pdf")  # noqa: E501
             )
-            print(response.json())
+        )
+
+    # db name is tenant1
+    db_name = "test_tenant1"
+    params = {
+        "db_name": db_name
+    }
+
+    # Make the POST request with the list of files
+    response = requests.post(
+        "http://0.0.0.0:8000/upload/",
+        files=files,
+        params=params
+    )
+
+    print(response.json())
+
+    # Close the files after the request
+    for _, (filename, file, _) in files:
+        file.close()  # Ensure each file is closed after the request
